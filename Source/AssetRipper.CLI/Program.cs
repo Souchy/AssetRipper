d502858566af72d48bb614d2bafd1e69c85de6e5
Souchy <robyn.g@hotmail.fr>
2025-07-01 12:47:03 -0400

    AssetRipper.CLI start

---
+++ Source/AssetRipper.CLI/Program.cs
@@ -0,0 +1,21 @@
+﻿using AssetRipper.Export.UnityProjects.Configuration;
+using AssetRipper.GUI.Web;
+
+namespace AssetRipper.CLI;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+
+		//Settings? settings = Settings.Parse(args);
+
+
+		GameFileLoader.Settings.LoadFromDefaultPath();
+		GameFileLoader.Settings.SaveToDefaultPath();
+		//SerializedSettings.Load("");
+
+		GameFileLoader.LoadAndProcess([settings.PathInput]);
+	}
+}
---
+++ Source/AssetRipper.CLI/Settings.cs
@@ -0,0 +1,23 @@
+﻿using Ookii.CommandLine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AssetRipper.CLI;
+
+[GeneratedParser]
+[ParseOptions(IsPosix = true)]
+internal partial class Settings
+{
+	public string UnityVersion { get; set; } = "2020.3.0f1";
+
+	[CommandLineArgument]
+	[Description("Path to input file or directory")]
+	public string PathInput { get; set; }
+
+	[CommandLineArgument]
+	[Description("Path to output directory.")]
+	public string PathOutputDir { get; set; }
+
+}
